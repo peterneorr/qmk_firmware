e2bdeaf0ddc190e1d35078e723f666d8a7462e3f
peterneorr <peterneorr@gmail.com>
2025-02-09 20:51:49 -0800

    tweaks

--- keyboards/handwired/emma46/keymaps/default/keymap.c
+++ keyboards/handwired/emma46/keymaps/default/keymap.c
@@ -59,40 +59,79 @@ const uint16_t PROGMEM to_qwerty1[] = {KC_ESC, KC_Q, COMBO_END};
 const uint16_t PROGMEM to_qwerty2[] = {KC_ESC, KC_QUOT, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(to_dvorak1, PDF(_DVRK)),   // Press ESC then D to switch to Dvorak
-    COMBO(to_dvorak2, PDF(_DVRK)),   // Press ESC then E to switch to Dvorak
-    COMBO(to_qwerty1, PDF(_BASE)),   // Press ESC then Q to switch to Qwerty
-    COMBO(to_qwerty2, PDF(_BASE))    // press ESC then ' to switch to Qwerty
+    COMBO(to_dvorak1, PDF(_DVRK)),   // Press ESC and D to switch to Dvorak
+    COMBO(to_dvorak2, PDF(_DVRK)),   // Press ESC and E to switch to Dvorak
+    COMBO(to_qwerty1, PDF(_BASE)),   // Press ESC and Q to switch to Qwerty
+    COMBO(to_qwerty2, PDF(_BASE))    // press ESC and ' to switch to Qwerty
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+        switch (keycode) {
+          case KC_BSPC: {
+            static uint16_t registered_key = KC_NO;
+            if (record->event.pressed) {  // On key press.
+              const uint8_t mods = get_mods();
+      #ifndef NO_ACTION_ONESHOT
+              uint8_t shift_mods = (mods | get_oneshot_mods()) & MOD_MASK_SHIFT;
+      #else
+              uint8_t shift_mods = mods & MOD_MASK_SHIFT;
+      #endif  // NO_ACTION_ONESHOT
+              if (shift_mods) {  // At least one shift key is held.
+                registered_key = KC_DEL;
+                // If one shift is held, clear it from the mods. But if both
+                // shifts are held, leave as is to send Shift + Del.
+                if (shift_mods != MOD_MASK_SHIFT) {
+      #ifndef NO_ACTION_ONESHOT
+                  del_oneshot_mods(MOD_MASK_SHIFT);
+      #endif  // NO_ACTION_ONESHOT
+                  unregister_mods(MOD_MASK_SHIFT);
+                }
+              } else {
+                registered_key = KC_BSPC;
+              }
+      
+              register_code(registered_key);
+              set_mods(mods);
+            } else {  // On key release.
+              unregister_code(registered_key);
+            }
+          } return false;
+      
+          // Other macros...
+        }
+      
+        return true;
+      }
+
+    
     // layouts array for 13x4 matrix
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_emma46(  
-        KC_ESC,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_BSPC,  //12 keys
+        KC_ESC,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,  //12 keys
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘    
         KC_TAB,      KC_A,   KC_S,   KC_D,   KC_F,   KC_G,           KC_H,   KC_J,   KC_K,   KC_L,  KC_SCLN, KC_QUOT, //12 keys
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘    
         F_PGUP,      SFT_Z,  KC_X,   KC_C,   KC_V,   MSC_B,          MSC_B,  KC_N,   KC_M,  KC_COMM, KC_DOT,SFT_SLSH,        KC_UP, //13 keys
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘       └───────┘    
-        G_PGDN,                    KC_LCTL,KC_LALT, KC_ENT,         KC_SPC,         KC_RALT,                        KC_LEFT,KC_DOWN, KC_RIGHT  //9 keys
+        G_PGDN,                    KC_LCTL,KC_LALT, KC_ENT,         KC_SPC,         KC_BSPC,                        KC_LEFT,KC_DOWN, KC_RIGHT  //9 keys
 //     └───────┘                   └───────┴───────┴───────┘       └───────────────┴───────┘                        └──────┴───────┴────────┘        
     ),
     [_DVRK] = LAYOUT_emma46(  //DVORAK layout
-        KC_ESC,     KC_QUOT,KC_COMM,KC_DOT,  KC_P,   KC_Y,           KC_F,   KC_G,   KC_C,   KC_R,   KC_L,  KC_BSPC,  //12 keys
+        KC_ESC,     KC_QUOT,KC_COMM,KC_DOT,  KC_P,   KC_Y,           KC_F,   KC_G,   KC_C,   KC_R,   KC_L,  KC_SLSH,  //12 keys
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘    
         KC_TAB,      KC_A,   KC_O,   KC_E,   KC_U,   KC_I,           KC_D,   KC_H,   KC_T,   KC_N,   KC_S,  KC_MINS, //12 keys
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘    
         F_PGUP,    SFT_SCLN, KC_Q,   KC_J,   KC_K,   MSC_X,          MSC_X,  KC_B,   KC_M,  KC_W,    KC_V,  SFT_Z,           KC_UP, //13 keys
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘       └───────┘    
-        G_PGDN,                    KC_LCTL,KC_LALT, KC_ENT,         KC_SPC,         KC_RALT,                        KC_LEFT,KC_DOWN, KC_RIGHT  //9 keys
+        G_PGDN,                    KC_LCTL,KC_LALT, KC_ENT,         KC_SPC,         KC_BSPC,                        KC_LEFT,KC_DOWN, KC_RIGHT  //9 keys
 //     └───────┘                   └───────┴───────┴───────┘       └───────────────┴───────┘                        └──────┴───────┴────────┘        
     ),
     [_MISC] = LAYOUT_emma46(  // MISC layer - numbers, symbols, navigation
-        KC_GRV,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,           KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_DEL,  
+        KC_GRV,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,           KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_TILD,  
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘    
         KC_CAPS,    KC_EXLM, KC_AT, KC_HASH,KC_DLR, KC_PERC,        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSLS, 
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘    
-         ____,      KC_LPRN,KC_RPRN,KC_LBRC,KC_RBRC, KC_EQL,        KC_LT,  KC_GT,  KC_LCBR,KC_RCBR,KC_MINS,KC_PIPE,        KC_INS, 
+         ____,      KC_LPRN,KC_RPRN,KC_LBRC,KC_RBRC, KC_EQL,        KC_PLUS,KC_MINS,KC_LCBR,KC_RCBR, ____,  KC_PIPE,        KC_INS, 
 //     └───────┘   └───────┴───────┴───────┴───────┴───────┘       └───────┴───────┴───────┴───────┴───────┴───────┘       └───────┘    
          ____,                       ____,   ____,   ____,           ____,           ____,                         KC_HOME, KC_DEL, KC_END  
 //    └───────┘                   └───────┴───────┴───────┘        └───────────────┴───────┘                        └──────┴───────┴────────┘        
